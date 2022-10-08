cda12b715813917be435168db82f2e18cc5abddf
pienk <kisuk623@gmail.com>
2022-10-08 14:22:47 +0900

    [ADD] 유저, 포스트 정보, nav 자동완성, 검색 결과 표시

---
+++ controllers/searchController.js
@@ -0,0 +1,39 @@
+const { searchService } = require("../services");
+const { asyncWrap } = require("../middleware/errorHandler");
+const { BaseError } = require("../util/error");
+
+
+const getPostsByNav = asyncWrap( async(req, res) => {
+    const { post } = req.query
+
+    if ( !post ) throw new BaseError("KEY_ERROR", 400);
+    const postInfo = await searchService.getPostsByNav(post);
+
+    return res.status(200).json({ postInfo });
+})
+
+const getProducts = asyncWrap( async(req, res) => {
+    const { product } = req.query;
+
+    if ( !product ) throw new BaseError("KEY_ERROR", 400);
+    const products = await searchService.getProducts( product );
+
+    return res.status(200).json({ products });
+})
+
+const getPostLists = asyncWrap( async(req, res) => {
+    const userId = req.user.id;
+    const { post } = req.query;
+
+    if ( !post ) throw new BaseError("KEY_ERROR", 400);
+    const posts = await searchService.getPostLists( post, userId );
+
+    return res.status(200).json({ posts });
+})
+
+
+module.exports = {
+    getPostsByNav,
+    getProducts,
+    getPostLists
+}
--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,6 @@ const { BaseError } = require("../util/error");
 const loginRequired = async (req, res, next) => {
     try {
         const token = req.headers.authorization;
-        
         const { user_id } = jwt.verify(token, process.env.TOKKENSECRET);
 
         const user = await userDao.getUserById( user_id )
@@ -16,10 +15,32 @@ const loginRequired = async (req, res, next) => {
         req.user = user;
         return next();
     } catch (err) {
-        const error = new BaseError("INVALID_TOKEN", 400);
-        next(error)
+        next( new BaseError("INVALID_TOKEN", 400) );
+    }
+}
+
+const getUserId = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization;
+        if ( !token ) {
+            req.user = 0;
+            return next();
+        }
+
+        const { user_id } = jwt.verify(token, process.env.TOKKENSECRET);
+
+        const user = await userDao.getUserById( user_id )
+        req.user = user;
+        
+        return next();
+    } catch(err) {
+        req.user = 0
+        next();
     }
 }
 
 
-module.exports = loginRequired
+module.exports = { 
+    loginRequired,
+    getUserId
+}
