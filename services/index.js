09f17723fc0084b7afbc3e05d58a9875fde29050
pienk <kisuk623@gmail.com>
2022-11-09 17:09:32 +0900

    필요없는 부분 삭제

--- services/index.js
+++ services/index.js
@@ -2,19 +2,15 @@ const pingService = require("./pingService");
 const userService = require("./userService");
 const profileService = require("./profileService");
 const searchService = require("./searchService")
-const likeService = require("./likeService")
 const postService = require("./postService");
 const commentService = require("./commentService");
-const postingService = require("./postingService")
 
 
 module.exports = {
     pingService,
     userService,
-    likeService,
     profileService,
     searchService,
     postService,
     commentService,
-    postingService
 }
